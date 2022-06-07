d8bc47c4bc2d4c835e9307f19b4819dff2d1be9c
EC2 Default User <gakuto404@gmail.com>
2022-06-07 14:34:04 +0000

    [update]admin関連一旦保存

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,10 +1,37 @@
 class Admin::ReviewsController < ApplicationController
+
+
   def index
+    @genres = Genre.all
+    @reviews = Review.all
   end
 
   def show
+    @review = Review.find(params[:id])
+    @user = @review.user
   end
 
   def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+     @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to admin_review_path(@review.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Reviews.find(params[:id])
+    @review.destroy
+    redirect_to admin_reviews_path
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:image, :title, :item_name, :genre_id, :manufacture_name, :impressions, :purchase_price, :purchase_source)
   end
 end
