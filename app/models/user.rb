c7bc933029f1172f339c3d9339e0392646ae76d3
EC2 Default User <gakuto404@gmail.com>
2022-06-21 12:22:10 +0000

    ゲストログイン調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,9 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.nick_name = "ゲスト"
+      user.last_name = "ゲスト"
+      user.first_name = "ユーザー"
     end
   end
 
