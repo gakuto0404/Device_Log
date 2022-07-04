4066957e7f2026cfc7e8d696647f28bad53cbe28
EC2 Default User <gakuto404@gmail.com>
2022-07-04 02:17:25 +0000

    [finish]部分テンプレート追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,11 +2,10 @@ class Public::HomesController < ApplicationController
 
   def top
     @genres = Genre.all
-    #@reviews = Review.all
     @search_params = review_search_params
     @reviews = Review.search(@search_params).includes(:genre)
   end
-  
+
   private
 
   def review_search_params
